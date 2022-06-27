5d6ae32cc5b92a33941f85e8c4951d3e0d51d541
ClakySecomd <89134670+ClakySecomd@users.noreply.github.com>
2022-06-27 17:34:15 +0200

    Implemented game env

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,5 +1,10 @@
 use device_query::{DeviceQuery, DeviceState, Keycode};
 
+const HEIGHT: usize = 25;
+const WIDTH: usize = 17;
+
+static SCREEN: [char; WIDTH * HEIGHT] = ['`'; WIDTH * HEIGHT];
+
 fn main() {
     println!("Let the game begin...");
 
@@ -11,6 +16,27 @@ fn start()
     // here we will define the game environment
     // and how the map will be updated as the 
     // bird falls
+
+    draw_begining();
+}
+
+fn draw_begining()
+{
+    let mut number: usize = 0;
+    for _i in 0..HEIGHT
+    {
+        for _j in 0..WIDTH
+        {
+            match number
+            {
+                24 => print!("# "),
+
+                _ => print!("{:} ", SCREEN[number]),
+            }
+            number += 1;
+        }
+        println!();
+    }
 }
 
 fn input()
@@ -23,7 +49,7 @@ fn input()
 
         for key in keys.iter()
         {
-            println!("Pressed key: {:?} {:?}", key);
+            println!("Pressed key: {:?}", key);
         }
     }
 }
